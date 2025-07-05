66f20f9c1a7e3453352fbd940fda9cb312ce21d6
Pavithra354 <naralapavithra@gmail.com>
2025-07-05 19:31:31 +0530

    commit

---
+++ Csharp_step2/StaticvsNonstatic/Members.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace StaticAndNonStaticMembers
+{
+    public static class EmployeeStaticLogger
+    {
+        public static int EmployeesList = 0;
+
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine($"[LOG] {message}");
+            EmployeesList = EmployeesList + 1;
+        }
+
+        public static int GetMessageCount()
+        {
+            return EmployeesList;
+        }
+    }
+
+    public class EmployeeNonStaticLogger
+    {
+        public int EmployeesList = 0;
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"[LOG] {message}");
+            EmployeesList = EmployeesList + 1;
+        }
+
+        public int GetMessageCount()
+        {
+            return EmployeesList;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            EmployeeStaticLogger.LogMessage("John has joined on 01-10-2021");
+            EmployeeStaticLogger.LogMessage("Robert has joined on 01-11-2021");
+
+            Console.WriteLine($"Total static log messages: {EmployeeStaticLogger.GetMessageCount()}");
+        }
+    }
+}
