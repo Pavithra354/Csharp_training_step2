cffe445c7af5818934242a1ace2a6cdf1209b613
Pavithra354 <naralapavithra@gmail.com>
2025-07-05 20:43:44 +0530

    commit

---
+++ Csharp_step2/StaticvsNonstatic/Simplecode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SimpleCode
+{
+    class BasicCalculator
+    {
+        public static int number1 = 20;
+        public static int number2 = 10;
+
+        public static void Addition()
+        {
+            int sum = number1 + number2;
+            Console.WriteLine($"Addition: {sum}");
+        }
+
+        public static void Multiplication()
+        {
+            int product = number1 * number2;
+            Console.WriteLine($"Multiplication: {product}");
+        }
+
+        public void Subtraction()
+        {
+            int diff = number1 - number2;
+            Console.WriteLine($"Subtraction: {diff}");
+            Division();
+        }
+
+        public void Division()
+        {
+            if (number2 != 0)
+            {
+                int quotient = number1 / number2;
+                Console.WriteLine($"Division: {quotient}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero!");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Addition();
+                Multiplication();
+
+                BasicCalculator calc = new BasicCalculator();
+                calc.Subtraction();
+                calc.Division();
+                
+                Multiplication();
+            }
+        }
+    }
+}
